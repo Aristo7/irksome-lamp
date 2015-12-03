b4a085a62fa1f0112b02029cd39490cd0d3d0404
Aristo7 <mr.olexander@gmail.com>
2015-12-02 21:16:09 -0500

    Got white lamp loaded in with collada.

--- main.js
+++ main.js
@@ -12,18 +12,52 @@ document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 camera.position.z = 5;
+
+var model_lamp;
+
+var loader = new THREE.ColladaLoader();
+
+loader.load(
+    // resource URL
+    'models/Model_for_Render.dae',
+    // Function when resource is loaded
+    function ( collada ) {
+        scene.add( collada.scene );
+        model_lamp = collada.scene;
+
+        init();
+        render();
+    },
+    // Function called when download progresses
+    function ( xhr ) {
+        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+    }
+);
+
+function init(){
+    // Add some lights to the scene
+    var directionalLight = new THREE.DirectionalLight(0xeeeeee , 1.0);
+    directionalLight.position.x = 1;
+    directionalLight.position.y = 0;
+    directionalLight.position.z = 0;
+    scene.add( directionalLight );
+
+    var directionalLight2 = new THREE.DirectionalLight(0xeeeeee, 2.0);
+    // A different way to specify the position:
+    directionalLight2.position.set(-1, 0, 1);
+    scene.add( directionalLight2 );
+
+    var ambientLight = new THREE.AmbientLight(0xBBBBBB);
+    scene.add(ambientLight);
+}
 
 var render = function () {
     requestAnimationFrame( render );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    model_lamp.rotation.x += 0.01;
+    model_lamp.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 };
-
-render();
